ac204179afb8fea4355ee515181a2b80ce1df8d2
Thinkofdeath <thethinkofdeath@gmail.com>
2013-12-16 22:25:56 +0000

    .

--- spigotbridge.py
+++ spigotbridge.py
@@ -5,7 +5,7 @@ __module_version__ = "0.1"
 __module_description__ = "Changes how BridgeBabe's messages are displayed"
 
 def chanmessage(word, word_eol, userdata):
-	if hexchat.nickcmp(hexchat.strip(word[0]), "B") == 0 and word[1][0] != "(":
+	if hexchat.nickcmp(hexchat.strip(word[0]), "B") == 0 and word[1][0] == "(":
 		w = word[1][1:]
 		name = "." + w[:w.index(')')]
 		message = w[w.index(')') + 2:]
