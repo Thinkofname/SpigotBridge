6463260db113ddf13e7ac66c3d34360261d86a06
Thinkofdeath <thethinkofdeath@gmail.com>
2014-01-03 11:09:00 +0000

    Support B and BridgeBabe

--- spigotbridge.py
+++ spigotbridge.py
@@ -7,14 +7,13 @@ __module_description__ = "Changes how BridgeBabe's messages are displayed"
 def chanmessage(word, word_eol, userdata):
 	nick = word[0]
 	message = word[1]
-	if hexchat.nickcmp(hexchat.strip(nick), "B") == 0 and message.startswith("("):
-		
+	if (hexchat.nickcmp(hexchat.strip(nick), "B") == 0 or hexchat.nickcmp(hexchat.strip(nick), "BridgeBabe") == 0) and message.startswith("("):		
 		name = "." + message[1:message.index(')')]
 		message = message[message.index(')') + 2:]
 		mode = name[1] if name[1] in "@+&~%" else ""
 
 		hexchat.emit_print(userdata, name, message, mode)
-		return hexchat.EAT_HEXCHAT
+		return hexchat.EAT_ALL
 	return hexchat.EAT_NONE
 
 hexchat.hook_print("Channel Message", chanmessage, "Channel Message")
--- spigotbridge_xchat_nosym.py
+++ spigotbridge_xchat_nosym.py
@@ -7,15 +7,14 @@ __module_description__ = "Changes how BridgeBabe's messages are displayed"
 def chanmessage(word, word_eol, userdata):
 	nick = word[0]
 	message = word[1]
-	if xchat.nickcmp(xchat.strip(nick), "B") == 0 and message.startswith("("):
-		
+	if (xchat.nickcmp(xchat.strip(nick), "B") == 0 or xchat.nickcmp(xchat.strip(nick), "BridgeBabe") == 0) and message.startswith("("):		
 		name = "." + message[1:message.index(')')]
 		message = message[message.index(')') + 2:]
 		mode = name[1] if name[1] in "@+&~%" else ""
 		name = name if mode == "" else "." + name[2:]
 
 		xchat.emit_print(userdata, name, message, mode)
-		return xchat.EAT_XCHAT
+		return xchat.EAT_ALL
 	return xchat.EAT_NONE
 
 xchat.hook_print("Channel Message", chanmessage, "Channel Message")
